695dceb79f9a1d56c5c96899900bd243d5442f9c
WujekAtom <spolecznosc.kapitana.commodore@gmail.com>
2018-07-03 00:20:55 +0200

    Bubble sorting/ Python implementation

--- bubbleSort.py
+++ bubbleSort.py
@@ -9,4 +9,21 @@ def randomData():
         data.append(randint(0,100))
     return data
 
-print(randomData())
+dataToSort = randomData()
+
+def bubbleSorting(dataIn):
+    sortedData = False
+    while True:
+        sortedData = False
+        for i in range(len(dataIn) -1):
+            if dataIn[i] > dataIn[i+1]:
+                # Replacing elements
+                tmp = [dataIn[i], dataIn[i+1]]
+                dataIn[i] = tmp[1]
+                dataIn[i+1] = tmp[0]
+                sortedData = True
+        if sortedData == False:
+            return dataIn
+
+print(dataToSort)
+print(bubbleSorting(dataToSort))
