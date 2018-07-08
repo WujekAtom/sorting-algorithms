2670d3a0fb76318caa2a9239e23ab66c51cf224f
wujekatom <spolecznosc.kapitana.commodore@gmail.com>
2018-07-08 21:41:26 +0200

    Insert sort and insert sort with binary search in Python

--- insertSort.py
+++ insertSort.py
@@ -7,10 +7,9 @@ def randomData():
         data.append(randint(0,100))
     return data
 
-data = [78, 51, 70, 93, 80, 34, 27, 90, 63, 44, 34, 35, 33, 41, 60, 37, 60, 60, 3]
 dataRandom = randomData()
-data2 = [2,9,4,3,5,6,7]
-data3 = [43, 69, 8, 25, 90, 16, 0, 51, 72]
+
+"""
 # 0. Saving 45 (before last element)
 # 1. [20, 9, 45, 6] checking if 45 > 6, if yes than move 6 to left
 # 2. [20, 9, 6, 45] Decreasing loop counter, save 9. If 9 >6, if yes, move 6 left
@@ -19,6 +18,8 @@ data3 = [43, 69, 8, 25, 90, 16, 0, 51, 72]
 # 5. [6,_, 9, 45] 20 > 9, move 9 left
 # 6. [6, 9, _ , 45] 20 >45
 # 7. [6, 9, 20, 45]
+
+"""
 def insertSorting(dataToSort):
     n = len(dataToSort) - 1  # how many index
     j = len(dataToSort) - 2 # starts from the before-last element
@@ -36,7 +37,16 @@ def insertSorting(dataToSort):
     return dataToSort
 
 
-# IN PROGRESS
+"""
+# INSERT SORT WITH BINARY SEARCHING
+# This method takes subarray on the right side of current element [TMP] to sort
+# and is searching middle element of the subarray. When TMP is bigger than middle, then it divides the subarray
+# into smaller subarray on the right side of MIDDLE element. Otherwise on the left side.
+# It causes to diving the subarray into smaller subarray until it contains 1 element.
+# Each time TMP is compared with MIDDLE so at the end we get the last element of subsubarray and by comparing
+# TMP with this last one we find the position where TMP must be inserted.
+
+"""
 def insertSortBinarySelect(dataToSort):
 
     n = len(dataToSort) -1 # for loop, number of elements - 1
@@ -46,48 +56,51 @@ def insertSortBinarySelect(dataToSort):
         # starts from before last element
         tmp = dataToSort[i]     # saving element that will be insert in proper place
 
-        #looking for the place in array when tmp can be add
-        indxP = i   # start of array when tmp will be insert
-        indxK = n   # end of array when tmp will be insert
-        print("Checking: %s" %tmp)
-        while indxK - indxP > 1:    # while searching array has more than 1 element
-            middle = (indxP + indxK) // 2   # calculate middle element in array: [1,2,3] -> el: 2
-            print("Middle: %s" %dataToSort[middle])
-            # if tmp is bigger than middle element, than we are looking for a place
+        #looking for the place in subarray when tmp can be add
+        indxP = i + 1  # start of subarray from the element on the right of element to insert [current el, so called TMP]
+        indxK = n  # end of array
+
+        while indxK - indxP >= 1:    # dividing the array until we get one-element subarray
+            middle = (indxP + indxK) // 2   # calculate index of middle element in array: [1,2,3] -> el: 2
+
+            # if TMP is bigger than MIDDLE element, than we are looking for a place
             # to insert on the right side od middle - it means we will divide array from
-            # right side of the middle untill the array will contains 1 element
-            print("1. %s %s" %(indxP, indxK))
+            # right side of the MIDDLE. Dividing through the while loop until the subarray will contains 1 element
+            # We are changing indxP which is beginning of subarray to new beginning, on the right side of MIDDLE
+            # Otherwise we are looking on the left of MIDDLE, so the inxdP [begigning] stay, but the end needs
+            # to be change to the index of MIDDLE element
+
             if tmp > dataToSort[middle]:
-                indxP = middle
-                indxK = n
-                print("2. %s %s" % (indxP, indxK))
+                indxP = middle + 1
             else:
-                indxK = middle - 1
-                print("3. %s %s" % (indxP, indxK))
+                indxK = middle
         else:
-            print("Too short array. Tmp = %s" %tmp)
-            m = i
-            while tmp > dataToSort[indxP]:
-                #print("indxP: %s" %dataToSort[indxP])
-                print("m = %s" %m)
-                tmp2 = [tmp, dataToSort[indxP]]
-                dataToSort[m] = tmp2[1]
-                dataToSort[indxP] = tmp2[0]
-                tmp = dataToSort[m+1]
-                indxP += 1
 
-            print(dataToSort)
+            # While loop finished dividing the array [looking for the place to insert TMP]. We have indexes of begin
+            # and end of subarray. Now we are moving all elements to the left to make a place for insert TMP.
+            # For example:
+            # [1,7,2,3,4,5,6] -> checking the "7"element, so need to move the elements to the left:
+            # [1,2,3,4,5,6,6] -> Moved. Double 6 is ok, on the last position in next step will be add "7"
 
+            for k in range(i, indxP):
+                # k<n checking if we are not out of array's range
+                if k < n:
+                    dataToSort[k] = dataToSort[k+1]
 
-
-
-
-
+            # If TMP is bigger than found [last] element from subarray, then insert TMP after it...
+            if tmp > dataToSort[indxP]:
+                dataToSort[indxP] = tmp
+            else:
+                # ...otherwise before.
+                dataToSort[indxP-1] = tmp
     return dataToSort
+
+
 print("Sorting:")
-print(data3)
+
+print(dataRandom)
+
+# Uncomment below to use regular insert or with binary search
+
 #print(insertSorting(dataRandom))
-
-print(insertSortBinarySelect(data3))
-
-# [43, 69, 8, 25, 90, 16, 0, 51, 72]
+print(insertSortBinarySelect(dataRandom))
