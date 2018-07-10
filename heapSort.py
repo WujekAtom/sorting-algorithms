769baf0b0eaddaecdb317bc1b135e64dabba52a0
wujekatom <spolecznosc.kapitana.commodore@gmail.com>
2018-07-10 15:29:14 +0200

    In progres

--- heapSort.py
+++ heapSort.py
@@ -18,16 +18,21 @@ data2 = [7, 5, 9, 6, 7, 8, 10]
 def heapSort(dataToSort):
     tabLen = len(dataToSort)
 
+    # for i in range(1, tabLen):
+    #     j = i
+    #     if j%2:
+    #         k = j//2
+    #     else:
+    #         k = j //2 - 1
+    #     tmp = dataToSort[i]
+    #     print("j = %s\nk = %s\ntmp = %s" %(j,k,tmp))
+    #     if k> -1:
+    #         print(dataToSort[k])
+    #     print("\n")
+
     for i in range(1, tabLen):
         j = i
-        if j%2:
-            k = j//2
-        else:
-            k = j //2 - 1
-        tmp = dataToSort[i]
-        print("j = %s\nk = %s\ntmp = %s" %(j,k,tmp))
-        if k> -1:
-            print(dataToSort[k])
-        print("\n")
+        if dataToSort[j-1] < dataToSort[j*2]:   # Left node
+            pass
 print(data2)
 heapSort(data2)
